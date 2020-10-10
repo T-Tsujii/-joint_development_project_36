ed087aa5ef6791d483bf211759c6a529d402fee9
fumi238000 <s21231128@gmail.com>
2020-10-10 14:18:58 +0900

    レビュー箇所の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,15 +19,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])  
   end
   
-def update
-   user = User.find(params[:id])
-   user.update(user_params)
-end
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
 
-def destroy
-  user = User.find(params[:id])
-  user.destroy
-end
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
 
 
   private
