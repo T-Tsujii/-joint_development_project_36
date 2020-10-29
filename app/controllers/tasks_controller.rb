52268a454cb52e233256e5ed5320d7cdc7fa3f77
wakikazushi <arubinozyonna@gmail.com>
2020-10-29 20:32:47 +0900

    編集機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,16 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  # 編集を表示するメソッド
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :text)
