afe5648e3d5fe72278c87ba657c7064fa093006d
xxegawaxx <e.masashi.st+GitHub@gmail.com>
2020-11-07 00:29:17 +0900

    新規投稿機能を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+
+  # 新規投稿機能
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+  # ストロングパラメータ
+  def message_params
+    params.require(:message).permit(:title, :body)
+  end
+
 end
